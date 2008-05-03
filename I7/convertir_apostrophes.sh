a2feca50f71dd506aefaf140b2c9ceeb46b68257
farvardin <farvardin@bc513dbb-302d-0410-b37a-94943751d336>
2008-05-03 21:40:54 +0000

    encore une reorganisation des depots + suppressions fichiers obsoletes I7
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/informfr/informfr/informfr/trunk@106 bc513dbb-302d-0410-b37a-94943751d336

--- I7/convertir_apostrophes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Ce fichier permet de rajouter les crochets autour de tous les apostroques dans les mots
-# pour Inform 7
-cat fichier.txt | sed -e "s/S'/S[\']/g" | sed -e "s/N'/N[\']/g" | sed -e "s/L'/L[\']/g" | sed -e "s/C'/C[\']/g" | sed -e "s/D'/D[\']/g" | sed -e "s/J'/J[\']/g" | sed -e "s/M'/M[\']/g"  | sed -e "s/U'/U[\']/g" > fichier.tmp
-cat fichier.tmp | sed -e "s/s'/s[\']/g" | sed -e "s/ n'/ n[\']/g" | sed -e "s/l'/l[\']/g" | sed -e "s/ c'/ c[\']/g" | sed -e "s/d'/d[\']/g" | sed -e "s/j'/j[\']/g" | sed -e "s/m'/m[\']/g" | sed -e "s/u'/u[\']/g"  > fichier2.txt
-rm fichier.tmp
