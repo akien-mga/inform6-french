46f66b35f9dbb95e68c0a4b226a65bd8ffb0108c
hlabrand <hugo@hlabrande.fr>
2015-08-07 01:28:28 -0600

    French: oubli d'un espace dans IS__TX et ARE__TX

--- French.h
+++ French.h
@@ -927,9 +927,9 @@ Constant THOSET__TX   = "ces choses-là";
 Constant THAT__TX     = "cela";
 Constant OR__TX       = " ou ";
 Constant NOTHING__TX  = "rien";
-! Potentiellement utilisés par WriteListFrom (bit ISORARE_BIT), mais pas dans la bibli française
-Constant IS__TX       = "il y a";  ! Constant IS__TX       = "est ";   ! ancienne version
-Constant ARE__TX      = "il y a";  ! Constant ARE__TX      = "sont ";  ! ancienne version
+! Potentiellement utilisés par WriteListFrom (bit ISARE_BIT), mais pas dans la bibli française
+Constant IS__TX       = "il y a ";  ! Constant IS__TX       = "est ";   ! ancienne version
+Constant ARE__TX      = "il y a ";  ! Constant ARE__TX      = "sont ";  ! ancienne version
 ! Ne pas changer ceux-là, ils sont utilisés dans WriteAfterEntry, qui liste l'inventaire notamment
 !      (et sont déjà bien faits, cf ListMiscellany plus bas)
 Constant IS2__TX      = "";  ! dans/sur lequel " est"  => contenant/supportant
